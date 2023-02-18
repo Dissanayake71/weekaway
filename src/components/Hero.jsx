3a88415b8b25f9fb6a8ad186fb4ac52cf6af46e9
Dissanayake71 <85305564+Dissanayake71@users.noreply.github.com>
2023-02-19 01:08:31 +0530

    add image

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import image from '../images/2.png';
+
+const Hero = () => {
+  return (
+    <div>
+        <img src={image} alt="" />
+
+        <div>
+            <h1>Find Your Special Trip</h1>
+            <h2>With FlipFop</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
